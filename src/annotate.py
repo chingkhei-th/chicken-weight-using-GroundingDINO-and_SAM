da38c1f889e969943e937f558f6417fb98c4838c
chingkhei-th <chingkhei.thoudam@outlook.com>
2024-06-17 12:30:30 +0530

    remove weight prediction part and display only annotated images

--- src/annotate.py
+++ src/annotate.py
@@ -21,11 +21,15 @@ def annotate_and_save_images(
     """
     os.makedirs(output_dir, exist_ok=True)
 
+    # Loop over all images in the test directory
     for filename in os.listdir(image_dir):
         if filename.endswith(".jpg") or filename.endswith(".png"):
+            # Construct full path to the current image
             image_path = os.path.join(image_dir, filename)
+            # Load the image
             image = cv2.imread(image_path)
 
+            # Detect objects
             detections = grounding_dino_model.predict_with_classes(
                 image=image,
                 classes=enhance_class_name(classes),
@@ -33,34 +37,41 @@ def annotate_and_save_images(
                 text_threshold=text_threshold,
             )
 
+            # Convert detections to masks
             detections.mask = segment(
                 sam_predictor=sam_predictor,
                 image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB),
                 xyxy=detections.xyxy,
             )
 
+            # Annotate image with detections
             mask_annotator = sv.MaskAnnotator()
-            box_annotator = sv.BoxAnnotator()
-            labels = []
+            # box_annotator = sv.BoxAnnotator()
+            # labels = []
 
-            for mask, _, confidence, class_id, xyxy in zip(
-                detections.mask,
-                detections.area,
-                detections.confidence,
-                detections.class_id,
-                detections.xyxy,
-            ):
-                area = np.sum(mask)
-                weight = linear_regression_model.predict([[area]])[0]
-                label = f"{classes[class_id]} - Weight: {weight:.2f} g"
-                labels.append(label)
+            labels = [
+                f"{classes[class_id]} {confidence:0.2f}"
+                for _, _, confidence, class_id, _ in detections
+            ]
+
+            # for mask, _, confidence, class_id, xyxy in zip(
+            #     detections.mask,
+            #     detections.area,
+            #     detections.confidence,
+            #     detections.class_id,
+            #     detections.xyxy,
+            # ):
+            #     area = np.sum(mask)
+            #     weight = linear_regression_model.predict([[area]])[0]
+            #     label = f"{classes[class_id]} - Weight: {weight:.2f} g"
+            #     labels.append(label)
 
             annotated_image = mask_annotator.annotate(
                 scene=image.copy(), detections=detections
             )
-            annotated_image = box_annotator.annotate(
-                scene=annotated_image, detections=detections, labels=labels
-            )
+            # annotated_image = box_annotator.annotate(
+            #     scene=annotated_image, detections=detections, labels=labels
+            # )
 
             output_image_path = os.path.join(
                 output_dir, f"{os.path.splitext(filename)[0]}_annotated.jpg"
